42d6dd8bc874edd08ee98d0b22407c83503b9dce
Siddhant <siddhantrsen73938@gmail.com>
2024-02-04 23:16:17 +0530

    JSX Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
+import { Typography, AppBar, Container, Grow, Grid } from '@mui/material'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 function App() {
 
   return (
-    <>
-      <div>
-        APP
-      </div>
-    </>
+    <Container maxWidth="lg">
+      <AppBar position="static" color='inherit'>
+        <Typography variant="h2" align='center'>
+          Memories
+        </Typography>
+        <img src={memories} alt="icon" height="340"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
